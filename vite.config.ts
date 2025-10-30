4b7f32c40db4a0060d062d1d80683baf2ff52acd
Kamal Ouahbi <ouahbikamal00@gmail.com>
2025-10-30 17:57:47 +0100

    Fix MIME type and routing issues

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/SuiviParticipants/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  }
 })
